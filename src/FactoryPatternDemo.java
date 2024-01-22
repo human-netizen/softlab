d029b0054b64d7d81fb7048648dc69104b33e89c
Your Name <you@example.com>
2024-01-22 15:51:11 +0600

    checked hashcode

--- src/FactoryPatternDemo.java
+++ src/FactoryPatternDemo.java
@@ -3,5 +3,7 @@ public class FactoryPatternDemo {
         //ShapeFactory shapeFactory = new ShapeFactory();
         Shape shape1 = ShapeFactory.getShape("SQUARE");shape1.draw();
         Shape shape2 = ShapeFactory.getShape("RECTANGLE");shape2.draw();
+        System.out.println("hashcode of shape1: " + shape1.hashCode());
+        System.out.println("hashcode of shape2: " + shape2.hashCode());
     }
 }
