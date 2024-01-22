e22896ad9f07d9c8d0ad4303c5c0fff1dbe80aad
Your Name <you@example.com>
2024-01-22 15:43:38 +0600

    modified bug

--- src/FactoryPatternDemo.java
+++ src/FactoryPatternDemo.java
@@ -1,7 +1,7 @@
 public class FactoryPatternDemo {
     public static void main(String[] args) {
-        ShapeFactory shapeFactory = new ShapeFactory();
-        Shape shape1 = shapeFactory.getShape("CIRCLE");shape1.draw();
-        Shape shape2 = shapeFactory.getShape("RECTANGLE");shape2.draw();
+        //ShapeFactory shapeFactory = new ShapeFactory();
+        Shape shape1 = ShapeFactory.getShape("SQUARE");shape1.draw();
+        Shape shape2 = ShapeFactory.getShape("RECTANGLE");shape2.draw();
     }
 }
